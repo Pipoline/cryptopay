f6c210ce7b94ef1e95969c693f3c41b7b07213b2
Peter Gonda <peter@pipoline.com>
2019-11-03 12:44:43 +0100

    WIP

--- cryptopay/api.py
+++ cryptopay/api.py
@@ -2,14 +2,28 @@
 Crypto.com API module
 """
 
+import requests
+
 
 class Client (object):
 
-    endpoint = 'https://pay.crypto.com'
-    settings = {}
+    headers = {}
 
-    def __init__(self, settings=None):
-        self.settings = settings
+    def __init__(self, secret_key=None, api_endpoint_url='https://pay.crypto.com', timeout=30, session=None,
+                 ssl_verify=True):
+        self.timeout = timeout
+        self.req = session or requests
+        self.api_endpoint_url = api_endpoint_url
+        self.secret_key = secret_key
 
-    def login(self):
-        pass
+    def _call_api(self, location, method="GET", **kwargs):
+        if method == "GET":
+            self.req.get(f'{self.api_endpoint_url}/{location}',
+                         headers=self.headers,
+                         verify=self.ssl_verify,
+                         proxies=self.proxies,
+                         timeout=self.timeout
+                         )
+
+    def get_payments(self):
+        self._call_api()
