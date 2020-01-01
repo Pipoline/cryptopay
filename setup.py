8afbc77cfe258fc0cd2d9e9416dbd055579b3fcd
Peter Gonda <peter@pipoline.com>
2020-01-01 22:35:22 +0100

    Bump version: 0.0.2 → 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cryptopay",
-    version="0.0.2",
+    version="0.1.0",
     author="Peter Gonda",
     author_email="peter@pipoline.com",
     description="Crypto.com pay module",
