03d88ff8ae5587865ea779f5379dbac95754ba23
Peter Gonda <peter@pipoline.com>
2020-05-05 17:05:27 +0200

    Remove force gathering secret key from env variables

--- cryptopay/__init__.py
+++ cryptopay/__init__.py
@@ -7,9 +7,6 @@ from .exceptions import ApiKeyMissingException
 
 CRYPTOPAY_SECRET_KEY = os.environ.get('CRYPTOPAY_SECRET_KEY', None)
 
-if CRYPTOPAY_SECRET_KEY is None:
-    raise ApiKeyMissingException("All methods require API key")
-
 session = requests.Session()
 session.headers['Authorization'] = f"Bearer {CRYPTOPAY_SECRET_KEY}"
 
